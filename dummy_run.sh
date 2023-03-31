85589c1f026c0da021e3950fd92c803b9d7d5300
dj <zhongdejian.wtt@bytedance.com>
2023-03-31 11:10:32 +0800

    update

--- dummy_run.sh
+++ dummy_run.sh
@@ -1,3 +1,5 @@
+pip3 install -r requirements.txt
+
 mkdir -p data
 wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-vocab.json -O data/gpt2-vocab.json
 wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-merges.txt -O data/gpt2-merges.txt
